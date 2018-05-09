8e1a099f242367ef21246209e5c11c81ccff473b
Subrata Bauri <subrata@digitalaptech.com>
2018-05-09 15:32:53 +0530

    add bcmath php extension

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -26,7 +26,7 @@ sudo apt-get install php7.1 -y > /dev/null
 #echo "Installing PHP extensions"
 ## The following line is not tested
 # sudo apt-get install php7.1-common php7.1-dev php5-cli php7.1-fpm -y > /dev/null
-sudo apt-get install -y libapache2-mod-php7.1 php7.1-cli php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-mysql php7.1-mcrypt php7.1-zip -y > /dev/null
+sudo apt-get install -y libapache2-mod-php7.1 php7.1-cli php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-mysql php7.1-mcrypt php7.1-zip php7.1-bcmath -y > /dev/null
 sudo apt install php7.1-curl -y > /dev/null
 sudo apt install php7.1-soap -y > /dev/null
 sudo a2enmod rewrite
